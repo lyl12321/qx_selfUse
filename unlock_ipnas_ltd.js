6f429de1da4073590ce0746ae30e7de49d65e735
lqwq <1870458220@qq.com>
2022-08-31 09:26:41 +0800

    Create unlock_ipnas_ltd.js

---
+++ unlock_ipnas_ltd.js
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+
+[rewrite_local]
+http:\/\/ios\.ipnas\.ltd\/appstore url script-response-body https://raw.githubusercontent.com/lyl12321/qx_selfUse/main/unlock_ipnas_ltd.js
+
+[mitm] 
+
+hostname = ios.ipnas.ltd
+*
+*
+*/
+
+
+var body = $response.body;
+body = "{"code":0,"msg":"解锁码存在!!!!"}";
+$done({ 
+    body 
+});
